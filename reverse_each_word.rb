f2ef296e56315f4caeff72d2a7cbeace549049d6
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-05 14:09:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ sentence = ["Hello there, and how are you?"]
 
 
 def reverse_each_word(sentence)
-  sentence.collect do |letter| letter.reverse
+  sentence.split.collect do |letter| letter.reverse
   end
 end
